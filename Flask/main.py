cf865bc5fab8e670b9c00f8131853d5a48f4ba6c
tazi337 <tamara.zimmermann@otago.at>
2019-11-18 19:19:04 +0100

    add secret number game and recipe blog

--- Flask/main.py
+++ Flask/main.py
@@ -1,5 +1,6 @@
 import flask
 import random
+import model
 
 app = flask.Flask(__name__)
 
@@ -17,7 +18,15 @@ def portfolio():
 
 @app.route("/secret-number-game")
 def secret_number_game():
-    return flask.render_template("secret-number-game.html", secret_number=random.randint)
+    return flask.render_template("secret-number-game.html", secret_number=random.randint(0,10))
+
+@app.route("/blog")
+def blog():
+
+    recipe_1 = model.Recipe("Apfelstrudel", "Cut Apple Bake Sweet", "sweet dish")
+    recipe_2 = model.Recipe("Hamburger", "Fry Meat and Eat", "salty dish")
+    recipe_3 = model.Recipe("Soup", "Cut Carrots, Add Water", "salty dish")
+    return flask.render_template("blog.html", recipes=[recipe_1, recipe_2, recipe_3])
 
 if __name__ == '__main__':
     app.run()
---
+++ Flask/model.py
@@ -0,0 +1,6 @@
+class Recipe:
+    def __init__(self, name, description, taste):
+        self.name = name
+        self.description = description
+        self.taste = taste
+
