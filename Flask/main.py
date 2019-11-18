31999230a621e64dd631c1e7b0478aae9eeb9411
tazi337 <tamara.zimmermann@otago.at>
2019-11-18 18:37:27 +0100

    Upload Python

--- Flask/main.py
+++ Flask/main.py
@@ -1,10 +1,11 @@
 import flask
+import random
 
 app = flask.Flask(__name__)
 
 @app.route("/")
 def index():
-    return flask.render_template("index.html")
+    return flask.render_template("index.html", myname="Tamara")
 
 @app.route("/about")
 def about_me():
@@ -13,6 +14,10 @@ def about_me():
 @app.route("/portfolio")
 def portfolio():
     return flask.render_template("portfolio.html")
+
+@app.route("/secret-number-game")
+def secret_number_game():
+    return flask.render_template("secret-number-game.html", secret_number=random.randint)
 
 if __name__ == '__main__':
     app.run()
